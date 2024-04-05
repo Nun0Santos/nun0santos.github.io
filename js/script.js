512fb97010abecbd904aeb8cd50b998f55fd908f
nasantos <nuno.a.santos@criticalsoftware.com>
2024-04-05 18:26:25 +0100

    changed the worflows of the folders

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* ==== Typing animation ===== */
-var typed = new Typed(".typing",{
-    strings:["","Software Engineer", "Software Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-});
